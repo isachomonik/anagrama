30e22085f128d7e7b8eb747a6ed47d6c0586504b
Isac Homonik <dev.isachomonik@gmail.com>
2023-08-20 11:41:44 -0300

    creating repo

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int anagramQtd(int frase, int fraseTamanho)
+{
+    for(int i = 0; i <= fraseTamanho; i++)
+    {
+        
+    };
+};
+
+int anagram(int frase)
+{
+
+};
+
+int main()
+{
+
+    return 0;
+};
